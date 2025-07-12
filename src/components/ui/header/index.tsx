016ba97c705287c98adc8deb9556795076b9a015
mochizukiaiichiro <mochizukiaiichiro@gmail.com>
2025-07-13 07:39:49 +0900

    styledcomponentsの挿入位置の変更

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -2,20 +2,20 @@
 import Link from 'next/link'
 import styled from 'styled-components'
 
-const Header = () => {
-  const HeaderRoot = styled.header`
+const HeaderRoot = styled.header`
     padding: 0px 0px;  
     border-bottom: 1px solid;
   `
 
-  const Ul = styled.ul`
+const Ul = styled.ul`
     display: flex;
     padding: 0;
   `
-  const Li = styled.li`
+const Li = styled.li`
     list-style: none;
     margin-right: 20px;
   `
+const Header = () => {
   return (
     <>
       <HeaderRoot>
@@ -26,6 +26,9 @@ const Header = () => {
             </Li>
             <Li>
               <Link href="/header/app1">アプリ１</Link>
+            </Li>
+            <Li>
+              <Link href="/header/app2">アプリ２</Link>
             </Li>
           </Ul>
         </nav>
