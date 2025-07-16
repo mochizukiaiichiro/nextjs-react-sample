cf76a5b4044175ac978a06a905a429e80235f287
mochizukiaiichiro <mochizukiaiichiro@gmail.com>
2025-07-16 09:20:44 +0900

    トップページとヘッダーの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+"use client"
+import Link from "next/link"
+import styled from "styled-components"
+
+const Table = styled.table`
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  width: auto;
+
+  th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+  }
+  thead {
+    background-color: #f5f5f5;
+  }
+  thead th {
+    color: #333;
+    font-weight: bold;
+  }
+`
+
 export default function Page() {
-  return <h1>サンプルアプリ</h1>
+  const lists = [
+    { path: "app1", title: "簡易メモアプリ" },
+    { path: "app2", title: "APIの実装と動作確認" },
+    { path: "app3", title: "外部APIからのデータフェッチ確認" },
+  ]
+
+  return (
+    <>
+      <h1>サンプルアプリ</h1>
+      <Table>
+        <thead>
+          <tr><th>タイトル</th></tr>
+        </thead>
+        <tbody>
+          {lists.map(({ title, path }, index) => (
+            <tr key={index}><td><Link href={`/header/${path}`}> {title}</Link></td></tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  )
 }
--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -6,37 +6,12 @@ const HeaderRoot = styled.header`
     padding: 0px 0px;  
     border-bottom: 1px solid;
   `
-
-const Ul = styled.ul`
-    display: flex;
-    padding: 0;
-  `
-const Li = styled.li`
-    list-style: none;
-    margin-right: 20px;
-  `
 const Header = () => {
-  const links = [
-    { path: "/", label: "トップ" },
-    { path: "app1", label: "アプリ１" },
-    { path: "app2", label: "アプリ２" },
-    { path: "app3", label: "アプリ３" },
-  ];
-
   return (
     <>
       <HeaderRoot>
         <nav>
-          <Ul>
-            {links.map(({ path, label }, index) => {
-              const href = path === "/" ? "/" : `/header/${path}`;
-              return (
-                <Li key={index} >
-                  <Link href={href} >{label}</Link>
-                </Li>
-              )
-            })}
-          </Ul>
+          <Link href="/">トップ</Link>
         </nav>
       </HeaderRoot>
     </>
