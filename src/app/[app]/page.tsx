cd6ba2562ee40bbf7c44e173fcfff31c69dc20b9
mochizukiaiichiro <mochizukiaiichiro@gmail.com>
2025-07-18 14:07:00 +0900

    generateStaticParams() の追加

--- src/app/[app]/page.tsx
+++ src/app/[app]/page.tsx
@@ -1,19 +1,28 @@
 import { JSX } from "react";
 import { appList } from "@/lib/apps";
+import { notFound } from "next/navigation";
 
 type Props = {
     params: { app: string };
 };
 
-// アプリ名とコンポーネントのマッピング
+// パスとコンポーネントのマッピング
 const apps = new Map<string, JSX.Element>();
 appList.forEach(({ id, component }) => {
     apps.set(id, component);
 });
 
+// 静的生成対象のパラメータ
+export async function generateStaticParams() {
+    return appList.map(({ id }) => ({ app: id }));
+}
+
 export default async function HeaderAppPage({ params }: Props) {
     const { app } = await params;
 
-    // 存在するアプリなら表示、なければエラーメッセージ
-    return apps.get(app) ?? <div>未定義のアプリです: {app}</div>
+    if (!apps.has(app)) {
+        notFound();
+    }
+
+    return apps.get(app)!;
 }
