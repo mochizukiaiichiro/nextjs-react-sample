69186f12d2f32c9db9dd3d1c9018d9c8af0628f6
mochizukiaiichiro <mochizukiaiichiro@gmail.com>
2025-07-20 12:38:59 +0900

    データ検索・詳細表示アプリ機能追加 1.複数項目での絞り込み 2.リセット機能 3.大文字小文字を吸収 4.索結果の件数表示 5.Enterキーで検索実行

--- src/components/application/app3/App.tsx
+++ src/components/application/app3/App.tsx
@@ -32,6 +32,10 @@ export const App = ({ Id }: { Id: string }) => {
   const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [website, setWebsite] = useState<string>("");
   const app = new Map(appMetaList.map(app => [app.id, app])).get(Id);
 
   React.useEffect(() => {
@@ -64,38 +68,69 @@ export const App = ({ Id }: { Id: string }) => {
   const onChangeName = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   }
+  const onChangeUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  }
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  }
+  const onChangeWebsite = (e: ChangeEvent<HTMLInputElement>) => {
+    setWebsite(e.target.value);
+  }
 
   //検索
   const onClickSearchButton = useCallback(() => {
-    if (name != "") {
-      setFilteredUsers(allUsers.filter((value) => value.name.toLowerCase().includes(name.toLowerCase())));      // 大文字小文字の違いを吸収
+    if ([name, username, email, phone, website].some(array => Boolean(array))) {
+      setFilteredUsers(allUsers.filter((user) =>
+        user.name.toLowerCase().includes(name.toLowerCase())
+        && user.username.toLowerCase().includes(username.toLowerCase())
+        && user.email.toLowerCase().includes(email.toLowerCase())
+        && user.phone.toLowerCase().includes(phone.toLowerCase())
+        && user.website.toLowerCase().includes(website.toLowerCase())
+      ));
     } else {
       setFilteredUsers(allUsers);
     }
-  }, [filteredUsers, name])
+  }, [filteredUsers, name, username, email, phone, website])
+
+  const onClickResetButton = () => {
+    setName("");
+    setUsername("");
+    setEmail("");
+    setPhone("");
+    setWebsite("");
+    setFilteredUsers(allUsers);
+  };
 
   return (
     <>
       <h1>{app?.title ?? "タイトル未定"}</h1>
       <p>{app?.description}</p>
       <div>
-        <label>name:</label>
-        <input name="name" value={name} type="text" onChange={onChangeName} />
-        <label>username:</label>
-        <input name="username" value={name} type="text" onChange={onChangeName} />
+        <p>検索項目</p>
+        <div>
+          <label>name:</label>
+          <input name="name" value={name} type="text" onChange={onChangeName} />
+          <label>username:</label>
+          <input name="username" value={username} type="text" onChange={onChangeUsername} />
+        </div>
+        <div>
+          <label>email:</label>
+          <input name="email" value={email} type="text" onChange={onChangeEmail} />
+          <label>phone:</label>
+          <input name="phone" value={phone} type="text" onChange={onChangePhone} />
+          <label>website:</label>
+          <input name="website" value={website} type="text" onChange={onChangeWebsite} />
+        </div>
+        <div>
+        </div>
+        <button type="submit" onClick={onClickSearchButton}>検索</button>
+        <button type="submit" onClick={onClickResetButton}>リセット</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
-      <div>
-        <label>email:</label>
-        <input name="email" value={name} type="text" onChange={onChangeName} />
-        <label>phone:</label>
-        <input name="phone" value={name} type="text" onChange={onChangeName} />
-      </div>
-      <div>
-        <input name="website" value={name} type="text" onChange={onChangeName} />
-      </div>
-      <button onClick={onClickSearchButton}>検索</button>
-      <button onClick={onClickSearchButton}>リセット</button>
-      {error && <p style={{ color: "red" }}>{error}</p>}
       <Table>
         <thead>
           <tr>
@@ -120,6 +155,7 @@ export const App = ({ Id }: { Id: string }) => {
           ))}
         </tbody>
       </Table>
+      <p>検索結果: {filteredUsers.length} 件</p>
       {name.length > 0 && filteredUsers.length === 0 && (<p>該当するユーザーが見つかりません</p>)}
     </>
   )
